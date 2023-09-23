9c3ab533e6451d953b2cc05440eaa720aeb7ba21
Arvind0302 <arvind.yadav.mec20@itbhu.ac.in>
2023-09-23 17:12:01 +0530

    header animation done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { motion } from 'framer-motion';
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 
@@ -10,12 +12,27 @@ const socialLinks: SocialLink[] = [
   { url: 'https://www.linkedin.com/in/arvind-yadav-88a8391bb/' },
   { url: 'https://twitter.com/ArvindY25557953' },
   { url: 'https://instagram.com/arvind.exe_' },
+  { url: 'https://www.facebook.com/profile.php?id=100057740764703'}
 ];
 
 export default function Header() {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div 
+      initial={{
+        x:-500,
+        opacity:0,
+        scale:0.5
+      }}
+      animate={{
+        x:0,
+        opacity:1,
+        scale:1
+      }}
+      transition={{
+        duration:1.3
+      }}
+      className="flex flex-row items-center">
         {/* Render social icons using map */}
         {socialLinks.map((socialLink, index) => (
           <SocialIcon
@@ -25,8 +42,22 @@ export default function Header() {
             bgColor='transparent'
           />
         ))}
-      </div>
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      </motion.div>
+      <motion.div
+            initial={{
+                x:+500,
+                opacity:0,
+                scale:0.5
+              }}
+              animate={{
+                x:0,
+                opacity:1,
+                scale:1
+              }}
+              transition={{
+                duration:1
+              }}
+      className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon 
         className="cursor-pointer"
         network='email'
@@ -34,7 +65,7 @@ export default function Header() {
         bgColor='transparent'
         />
        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-      </div>
+      </motion.div>
     </header>
   );
 }
