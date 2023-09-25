c86033aa444ba1d07cef5ed897953970a6eb1337
Arvind0302 <arvind.yadav.mec20@itbhu.ac.in>
2023-09-25 17:37:03 +0530

    skills done

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,21 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
       {/* <section id="hero" className="snap-start">
-       <Hero/>
+        <Hero />
       </section> */}
       <section id="about" className="snap-center">
-<About/>
+        <About />
       </section>
-      <section id="experience" className="snap-center">
-<Experience/>
-      </section>
+      {/* <section id="experience" className="snap-center">
+        <Experience />
+      </section> */}
       <section id="skills" className="snap-start">
-<Skills/>
+        <Skills />
       </section>
       <Footer />
     </main>
-  )
+  );
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Skill from "./Skill";
+import { motion } from "framer-motion";
+type Props = {};
 
-type Props = {}
+type SkillItem = {
+  name: string;
+  src: string;
+  directionLeft: boolean;
+  percentage: string;
+};
+
+const skillsData: SkillItem[] = [
+  {
+    name: "Skill 1",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "100%",
+  },
+  {
+    name: "Skill 2",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "85%",
+  },
+  {
+    name: "Skill 3",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "70%",
+  },
+  {
+    name: "Skill 4",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "95%",
+  },
+  {
+    name: "Skill 5",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "60%",
+  },
+  {
+    name: "Skill 6",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "75%",
+  },
+  {
+    name: "Skill 7",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "90%",
+  },
+  {
+    name: "Skill 8",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "80%",
+  },
+  {
+    name: "Skill 9",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "65%",
+  },
+  {
+    name: "Skill 10",
+    src: "https://cdn-icons-png.flaticon.com/512/919/919825.png",
+    directionLeft: true,
+    percentage: "55%",
+  },
+];
 
 export default function Skills({}: Props) {
   return (
-    <div>Skills</div>
-  )
+    <motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Skills
+      </h3>
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+        hover over a skill for current proficiency
+      </h3>
+      <div className="grid grid-cols-4 gap-5">
+        {skillsData.map((skill, index) => (
+          <Skill
+            key={index}
+            src={skill.src}
+            directionLeft={skill.directionLeft}
+            percentage={skill.percentage}
+          />
+        ))}
+      </div>
+    </motion.div>
+  );
 }
